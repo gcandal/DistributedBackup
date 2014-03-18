e2ccc25620552a85798967cd5606c2f052e6e349
Gabriel Candal <gabrielcandal@gmail.com>
2014-03-18 16:07:02 +0000

    foo

--- src/gui/StartWindow.java
+++ src/gui/StartWindow.java
@@ -33,15 +33,16 @@ public class StartWindow {
 	private JFrame frmDistributedBackupSystem;
 	private final int DEFAULT_MAX_SIZE = 50;
 	private final JFileChooser fc = new JFileChooser();
+	private String selectedFilePath = "";
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(final String[] args) {
-		
+
 		final Processor p = new Processor(args);
 		System.out.println(Message.bytesToHex(ChunkManager.fileToSHA256("forms-1.3.0.jar")));
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -52,9 +53,9 @@ public class StartWindow {
 				}
 			}
 		});
-		
+
 		p.process();
-		
+
 	}
 
 	/**
@@ -86,7 +87,7 @@ public class StartWindow {
 				ColumnSpec.decode("max(28dlu;default)"),
 				FormFactory.RELATED_GAP_COLSPEC,
 				ColumnSpec.decode("max(21dlu;default)"),},
-			new RowSpec[] {
+				new RowSpec[] {
 				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC,
@@ -115,22 +116,22 @@ public class StartWindow {
 				RowSpec.decode("max(74dlu;default):grow"),
 				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,}));
-		
+
 		JLabel mcAdressLabel = new JLabel("Multicast Address: " + args[0] +
 				":" + args[1]);
 		frmDistributedBackupSystem.getContentPane().add(mcAdressLabel, "4, 4");
-		
+
 		JLabel mdrAddressLabel = new JLabel("Multicast Restore Adress: " + args[2] +
 				":" + args[3]);
 		frmDistributedBackupSystem.getContentPane().add(mdrAddressLabel, "4, 6");
-		
+
 		JLabel mdbAddressLabel = new JLabel("Multicast Backup Address "  + args[4] +
 				":" + args[5]);
 		frmDistributedBackupSystem.getContentPane().add(mdbAddressLabel, "4, 8");
-		
+
 		final JLabel lblMaxsize = new JLabel("MaxSize ("+DEFAULT_MAX_SIZE+" MB)");
 		frmDistributedBackupSystem.getContentPane().add(lblMaxsize, "4, 10");
-		
+
 		final JSlider slider = new JSlider();
 		slider.setValue(DEFAULT_MAX_SIZE);
 		slider.addChangeListener(new ChangeListener() {
@@ -139,56 +140,63 @@ public class StartWindow {
 			}
 		});
 		frmDistributedBackupSystem.getContentPane().add(slider, "6, 10, 5, 1");
-		
+
 		JLabel lblCurrentsize = new JLabel("Current Size 0 MB");
 		frmDistributedBackupSystem.getContentPane().add(lblCurrentsize, "4, 12");
-		
+
 		final JLabel filePathLabel = new JLabel("New File Path:");
 		frmDistributedBackupSystem.getContentPane().add(filePathLabel, "4, 16");
-		
+
 		JButton browseFileButton = new JButton("Browse File...");
 		frmDistributedBackupSystem.getContentPane().add(browseFileButton, "6, 16");
-		
+
 		JSpinner spinner = new JSpinner(new SpinnerNumberModel(1, 1, 10, 1));
 		frmDistributedBackupSystem.getContentPane().add(spinner, "8, 16");
-		
+
 		JButton btnBackup = new JButton("Backup");
 		btnBackup.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		frmDistributedBackupSystem.getContentPane().add(btnBackup, "10, 16");
-		
+
 		JComboBox<String> comboBox = new JComboBox<String>();
 		frmDistributedBackupSystem.getContentPane().add(comboBox, "4, 20, 7, 1, fill, default");
-		
+
 		JButton btnRestore = new JButton("Restore...");
 		btnRestore.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		frmDistributedBackupSystem.getContentPane().add(btnRestore, "8, 22");
-		
+
 		JButton btnRemove = new JButton("Remove");
 		frmDistributedBackupSystem.getContentPane().add(btnRemove, "10, 22");
-		
+
 		JTextArea textArea = new JTextArea();
 		textArea.setEditable(false);
 		textArea.setLineWrap(true);
 		frmDistributedBackupSystem.getContentPane().add(textArea, "4, 26, 7, 1, fill, fill");
-		
+		fillTextArea(textArea);
+
 		browseFileButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-		        int returnVal = fc.showOpenDialog(frmDistributedBackupSystem.getContentPane());
+				int returnVal = fc.showOpenDialog(frmDistributedBackupSystem.getContentPane());
 
-			        if (returnVal == JFileChooser.APPROVE_OPTION) {
-			            File file = fc.getSelectedFile();
-			            
-			            if(file != null)
-			            	filePathLabel.setText("New file path: " + file.getAbsolutePath());
-			        }
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc.getSelectedFile();
+
+					if(file != null) {
+						selectedFilePath = file.getAbsolutePath();
+						filePathLabel.setText("New file path: " + file.getAbsolutePath());
+					}
+				}
 			}
 		});
+	}
+
+	private void fillTextArea(JTextArea textArea) {
+		
 	}
 
 }
