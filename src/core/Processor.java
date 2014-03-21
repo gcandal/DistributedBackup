ef27dd023325ddcf13611cef120a66007b116b43
Augusto Silva <ei11113@fe.up.pt>
2014-03-21 16:42:43 +0000

    outgoingQueue, added TODO comments

--- src/core/Processor.java
+++ src/core/Processor.java
@@ -7,6 +7,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import net.MulticastReceiver;
 import net.MulticastSender;
 
+//TODO geral -> adicionar syncronized a todos os acessos a chunks e myfiles
+
 public class Processor {
 
 	public static final float version = (float) 1.0;
@@ -22,6 +24,8 @@ public class Processor {
 												// alteracao mudou!
 	private ConcurrentLinkedQueue<Message> messageQueue;
 	private ConcurrentLinkedQueue<Chunk> waitingChunks;
+	private ConcurrentLinkedQueue<Message> outgoingQueue;
+	private HashMap<byte[], String> filesToBeRestored;// -> string is new name
 
 	public Processor(final String[] args) {
 		messageQueue = new ConcurrentLinkedQueue<>();
@@ -52,41 +56,53 @@ public class Processor {
 			Message msg = messageQueue.poll();
 			if (msg != null) { // empty queue
 
-				switch(msg.getMessageType())
-				{
-				case "PUTCHUNK":
-				{
+				switch (msg.getMessageType()) {
+				case "PUTCHUNK": {
 					processPutChunk(msg);
 				}
-				case "STORED":
-				{
+				case "STORED": {
 					processStored(msg);
 				}
-				case "GETCHUNK":
-				{
+				case "GETCHUNK": {
 					processGetChunk(msg);
 				}
-				case "CHUNK":
-				{
+				case "CHUNK": {
 					processChunk(msg);
 				}
-				case "DELETE":
-				{
+				case "DELETE": {
 					processDelete(msg);
 				}
-				case "REMOVED":
-				{
+				case "REMOVED": {
 					processRemoved(msg);
 				}
 				}
 
 			}
-			
-			Chunk chk = waitingChunks.poll(); // user mandou guardar - esperam confirmacoes
-			if(chk!=null)
-			{
-				if(chk.shouldResend())
-				{
+
+			Message out = outgoingQueue.poll();
+			if (out != null) { // empty queue
+
+				switch (out.getMessageType()) {
+				case "PUTCHUNK": {
+					mdbSender.send(out);
+				}
+				case "GETCHUNK": {
+					mcSender.send(out);
+				}
+				case "DELETE": {
+					mcSender.send(msg);
+				}
+				case "REMOVED": {
+					mcSender.send(msg);
+				}
+				}
+
+			}
+
+			Chunk chk = waitingChunks.poll(); // user mandou guardar - esperam
+												// confirmacoes
+			if (chk != null) {
+				if (chk.shouldResend()) {
 					sendPutChunk(chk);
 				} else
 					waitingChunks.add(chk);
@@ -94,128 +110,133 @@ public class Processor {
 		}
 	}
 
-	private void processRemoved(Message msg) { // is kept in line till random delay
-		
-		Chunk chk = chunks.get(Chunk.getHash(msg.getFileId(), msg.getChunkNo()));
-		if(chk==null)
+	private void processRemoved(Message msg) { // is kept in line till random
+												// delay
+
+		Chunk chk = chunks
+				.get(Chunk.getHash(msg.getFileId(), msg.getChunkNo()));
+		if (chk == null)
 			return;
-		
-		if(msg.ready())
-		{
+
+		if (msg.ready()) {
 			chk.removeHostWithChunk(msg.getSenderIp());
-			Message newMsg = new Message("PUTCHUNK",version,msg.getFileId(),msg.getChunkNo());
+			Message newMsg = new Message("PUTCHUNK", version, msg.getFileId(),
+					msg.getChunkNo());
 			newMsg.setReplicationDeg(chk.getReplicationDeg());
-			newMsg.setBody(chk.load());	
+			newMsg.setBody(chk.load());
 			mdbSender.send(newMsg);
-		}else
-			messageQueue.add(msg);	
-			
+		} else
+			messageQueue.add(msg);
+
 	}
 
 	private void processDelete(Message msg) {
 		Iterator<byte[]> it = chunks.keySet().iterator();
-		while(it.hasNext())
-		{
+		while (it.hasNext()) {
 			byte[] hash = it.next();
 			boolean belongs = true;
-			for(int i = 0; i < 256; i++)
-			{
-				if(hash[i] != msg.getFileId()[i])
-				{
+			for (int i = 0; i < 256; i++) {
+				if (hash[i] != msg.getFileId()[i]) {
 					belongs = false;
 					break;
 				}
 			}
-			
-			if(belongs)
-			{
+
+			if (belongs) {
 				it.remove();
-				//TODO Delete files
+				// TODO Delete files ---- to get byte[] for acess hashmap use
+				// Chunk.getHash(msg.getFileId(),
+				// msg.getChunkNo())
 			}
 		}
 	}
 
-	private void processChunk(Message msg) { //if chunk is received, kills waiting getchunk message
-		Chunk chk = chunks.get(Chunk.getHash(msg.getFileId(), msg.getChunkNo()));
-		if(chk==null)
+	private void processChunk(Message msg) { // if chunk is received, kills
+												// waiting getchunk message
+		Chunk chk = chunks
+				.get(Chunk.getHash(msg.getFileId(), msg.getChunkNo()));
+		if (chk == null)
 			return;
-		
-		if(chk.isMine()){
-			//TODO write/merge ???
-		} else
-		{ //Remove get chunk msg if in queue
-			for(Message m : messageQueue)
-			{
-				if(m.getMessageType()=="CHUNK" && Chunk.getHash(m.getFileId(), m.getChunkNo()).equals(chk.getHash()))
-				{
+
+		if (chk.isMine()) {
+			// TODO write to disk -> if all chunks are present, merge file with
+			// name in filesToBeRestored
+		} else { // Remove get chunk msg if in queue
+			for (Message m : messageQueue) {
+				if (m.getMessageType() == "CHUNK"
+						&& Chunk.getHash(m.getFileId(), m.getChunkNo()).equals(
+								chk.getHash())) {
 					messageQueue.remove(m);
 					break;
 				}
 			}
 		}
-			
+
 	}
 
-	private void processGetChunk(Message msg) { // is kept in line till random delay
-		if(msg.ready())
-		{
-			Chunk chk = chunks.get(Chunk.getHash(msg.getFileId(), msg.getChunkNo()));
-			if(chk==null)
+	private void processGetChunk(Message msg) { // is kept in line till random
+												// delay
+		if (msg.ready()) {
+			Chunk chk = chunks.get(Chunk.getHash(msg.getFileId(),
+					msg.getChunkNo()));
+			if (chk == null)
 				return;
-			
-			Message newMsg = new Message("CHUNK",version,msg.getFileId(),msg.getChunkNo());
-			newMsg.setBody(chk.load());	
+
+			Message newMsg = new Message("CHUNK", version, msg.getFileId(),
+					msg.getChunkNo());
+			newMsg.setBody(chk.load());
 			mdrSender.send(newMsg);
-		}else
+		} else
 			messageQueue.add(msg);
 	}
 
 	private void sendPutChunk(Chunk chk) {
-		Message msg = new Message("PUTCHUNK", version, chk.getFileId(), chk.getChunkNo());
+		Message msg = new Message("PUTCHUNK", version, chk.getFileId(),
+				chk.getChunkNo());
 		msg.setReplicationDeg(chk.getReplicationDeg());
 		msg.setBody(chk.load());
-		mcSender.send(msg);
+		outgoingQueue.add(msg);
 		chk.notifySent();
 	}
 
 	private void processStored(Message msg) {
 
 		byte[] key = Chunk.getHash(msg.getFileId(), msg.getChunkNo());
-		
+
 		Chunk c = chunks.get(key);
-		if(c!=null)
+		if (c != null)
 			c.addHostWithChunk(msg.getSenderIp());
-		
+
 	}
 
-	private void processPutChunk(Message msg) { // is kept in line till random delay
-		
-		if(msg.ready())
-		{
-			Message newMsg = new Message("STORED", version, msg.getFileId(), msg.getChunkNo());
+	private void processPutChunk(Message msg) { // is kept in line till random
+												// delay
+
+		if (msg.ready()) {
+			Message newMsg = new Message("STORED", version, msg.getFileId(),
+					msg.getChunkNo());
 			mcSender.send(newMsg);
-			
-			if(!myFiles.containsKey(msg.getFileId()))
-			{
-				Chunk chunk = new Chunk(msg.getFileId(), msg.getChunkNo(), msg.getReplicationDeg(),msg.getSenderIp());
-				//TODO Store chunk in disk
+
+			if (!myFiles.containsKey(msg.getFileId())) {
+				Chunk chunk = new Chunk(msg.getFileId(), msg.getChunkNo(),
+						msg.getReplicationDeg(), msg.getSenderIp());
+				// TODO Store chunk in disk
 				msg.getBody();
 				synchronized (chunks) {
 					chunks.put(chunk.getHash(), chunk);
 				}
 			}
 
-		}else
-		{
+		} else {
 			messageQueue.add(msg);
 		}
 	}
 
 	public void removeFile(String fileName) {
 
-		//TODO build message to be sent
-		//delete from disk
-		
+		// TODO build message to be sent and put in outgoingQueue
+		// delete from disk
+
 		byte[] sha = null;
 
 		for (Entry<byte[], String> pair : myFiles.entrySet()) {
@@ -232,20 +253,20 @@ public class Processor {
 	}
 
 	public void addFile(String fileName, int repDeg) {
-		//TODO break in chunks & add to chunks queue
-	//	sendPutChunk(chk);
+		// TODO break in chunks, add fileID to myfiles, add chunks to hash
+		// sendPutChunk(chk);
 	}
 
 	public void setSpaceLimit(int mbLimit) {
-		//TODO if necessary add msgs to sending queue?
+		// TODO if necessary add REMOVED msgs to outgoingqueue
 	}
-	
-	public void restoreFile(String fileName, String newLocation)
-	{
-		//TODO
+
+	public void restoreFile(String fileName, String newLocation) {
+		// TODO find chunks in hash, add CHUNK messages to outgoingqueue
+		// TODO add new name to filestoberestored hashmap
 	}
-	
-	public String[] getMyFiles(){
+
+	public String[] getMyFiles() {
 		String[] list;
 		synchronized (myFiles) {
 			list = (String[]) myFiles.values().toArray();
